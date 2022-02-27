8a74279d0571ae1b13affad35c70ebb597c2ff16
Honorine Ndom <honorinendzah@gmail.com>
2022-02-27 09:15:26 -0800

    changes done on CreateThoughtsTable

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -3,3 +3,19 @@ AWS.config.update({
     region: "us-east-2",
     endpoint: "http://localhost:8000"
   });
+  const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+  const params = {
+    TableName : "Thoughts",
+    KeySchema: [       
+      { AttributeName: "username", KeyType: "HASH"},  // Partition key
+      { AttributeName: "createdAt", KeyType: "RANGE" }  // Sort key
+    ],
+    AttributeDefinitions: [       
+      { AttributeName: "username", AttributeType: "S" },
+      { AttributeName: "createdAt", AttributeType: "N" }
+    ],
+    ProvisionedThroughput: {       
+      ReadCapacityUnits: 10, 
+      WriteCapacityUnits: 10
+    }
+  };
