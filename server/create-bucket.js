ecb52284f3ea3dfd7949ef39391d2aa5228c7702
Honorine Ndom <honorinendzah@gmail.com>
2022-02-27 05:20:01 -0800

    created CreateThoughtsTable.js

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 AWS.config.update({region: 'us-east-2'});
 
 // Create S3 service object
-const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const s3 = new AWS.S3({apiVersion: '2006-03-01',});
 
 // Create the parameters for calling createBucket
 var bucketParams = {
