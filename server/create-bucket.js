e23c3ae577c83ada97e10579a98188aba5f049c2
Honorine Ndom <honorinendzah@gmail.com>
2022-02-28 06:17:05 -0800

    added the endpoint to the AWS config

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -3,7 +3,10 @@ const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
 // Set the region
-AWS.config.update({region: 'us-east-2'});
+AWS.config.update({
+  region: "us-east-2",
+  endpoint: "http://localhost:8000"
+});
 
 // Create S3 service object
 const s3 = new AWS.S3({apiVersion: '2006-03-01',});
