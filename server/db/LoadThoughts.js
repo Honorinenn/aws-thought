c379e81e9ab1f8686f94b1ebd7af52fe479c26a1
Honorine Ndom <honorinendzah@gmail.com>
2022-02-28 06:48:16 -0800

    created params object

--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -5,6 +5,19 @@ AWS.config.update({
     region: "us-east-2",
     endpoint: "http://localhost:8000"
   });
+
   const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
   console.log("Importing thoughts into DynamoDB. Please wait.");
 const allUsers = JSON.parse(fs.readFileSync('./server/seed/users.json', 'utf8'));
+
+//Create params object//
+allUsers.forEach(user => {
+    const params = {
+      TableName: "Thoughts",
+      Item: {
+        "username": user.username,
+        "createdAt": user.createdAt,
+        "thought": user.thought
+      }
+    }
+});
