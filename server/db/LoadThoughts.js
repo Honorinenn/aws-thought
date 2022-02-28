f6c079633d5b55eb7a255c0dee13bddf2bb902be
Honorine Ndom <honorinendzah@gmail.com>
2022-02-28 06:51:36 -0800

    added the call to the database

--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -21,3 +21,12 @@ allUsers.forEach(user => {
       }
     }
 });
+
+//make a call to the database with the service interface object, dynamoDB//
+dynamodb.put(params, (err, data) => {
+    if (err) {
+      console.error("Unable to add thought", user.username, ". Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("PutItem succeeded:", user.username);
+    }
+});
