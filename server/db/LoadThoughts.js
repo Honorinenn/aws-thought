64babb3d0a195e1b1bce2175483a5d269ec768f3
Honorine Ndom <honorinendzah@gmail.com>
2022-03-31 19:42:51 -0700

    changes done on LoadThoughts.js

--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 AWS.config.update({
     region: "us-east-2",
     endpoint: "http://dynamodb.us-east-2.amazonaws.com"
-  });
+});
 
-  const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-  console.log("Importing thoughts into DynamoDB. Please wait.");
+const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+console.log("Importing thoughts into DynamoDB. Please wait.");
 const allUsers = JSON.parse(fs.readFileSync('./server/seed/users.json', 'utf8'));
 
 
