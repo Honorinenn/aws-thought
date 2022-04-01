e4f50e5e2ae86a2a61444ec8790081e06a37e767
Honorine Ndom <honorinendzah@gmail.com>
2022-03-31 18:29:53 -0700

    changes done on LoadThoughts.js

--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 AWS.config.update({
     region: "us-east-2",
-    endpoint: "http://dynamodb.us"
+    endpoint: "http://dynamodb.us-east-2.amazonaws.com"
   });
 
   const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
