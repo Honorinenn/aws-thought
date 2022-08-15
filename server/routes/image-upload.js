9d7241f82ae429670b264803873cf087ce2035b6
Honorine Ndom <honorinendzah@gmail.com>
2022-08-15 14:37:26 -0700

    added const storage image-upload.js

--- server/routes/image-upload.js
+++ server/routes/image-upload.js
@@ -2,3 +2,8 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const AWS = require('aws-sdk');
+const storage = multer.memoryStorage({
+    destination: function (req, file, callback) {
+      callback(null, '');
+    },
+  });
