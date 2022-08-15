ef4257b81de2cab344474a16994bc5d3ce00a34a
Honorine Ndom <honorinendzah@gmail.com>
2022-08-15 15:06:50 -0700

    updated params-config.js

--- server/utils/params-configs.js
+++ server/utils/params-configs.js
@@ -1,1 +1,15 @@
 const { v4: uuidv4 } = require('uuid');
+
+const params = (fileName) => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+  
+    const imageParams = {
+      // Replace the <My_Bucket_Name> with the name of your own S3 bucket
+      Bucket: 'user-images-44711ec5-1c5b-4415-8f06-11f249191107',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer,
+    };
+  
+    return imageParams;
+  };
